31c4e3fd03b6a78577cdc63aa73ac0b27f7f13c1
teo erfass josefsson <teo.erfassjosefsson@elev.ga.ntig.se>
2026-04-12 15:36:20 +0200

    feasf

--- SlutProjekt/Enemy2.cs
+++ SlutProjekt/Enemy2.cs
@@ -1,4 +1,9 @@
 public class Enemy2 : Fighter   // mer enemy type 2
 {
 
+public  void SpecialAttack(Fighter target)
+    {
+    target.TakeDamage(Health + 5);
+    }
+
 }
--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -3,6 +3,7 @@ Player player = new Player();
 
 while (true)
 {
+    Console.Clear();
     Console.WriteLine(@$"
 Välkommen {player.Name} till fight arenan här möter du bossar och kan levla upp i rank
 
@@ -36,6 +37,7 @@ skriv in siffran på det du vill ska hända!
 
         while (true)
         {
+            Console.Clear();
             Console.Write("loading");
             for(int i = 0; i<5;i++)
             {
@@ -55,17 +57,21 @@ skriv in siffran på det du vill ska hända!
             Console.ReadLine();
             if (enemy.Health <= 0)
             {
+                Console.Clear();
                 Console.WriteLine("Du vann!");
                 player.GainXp(50);
                 player.LevelUp();
                 player.ResetStats();
                 enemy.ResetStats();
+                Console.ReadLine();
                 break;
             }
 
             else if(player.Health <= 0)
             {
+                Console.Clear();
                 Console.WriteLine("Du förlorade...");
+                Console.ReadLine();
                 break;
             }
 
@@ -77,11 +83,16 @@ skriv in siffran på det du vill ska hända!
 
     else if (MenyVal == "2")
     {
+        Console.Clear();
         Console.WriteLine($"{player.Name}, Du har {player.Xp} Xp och är i level {player.Level}");
+        Console.WriteLine("");
+        Console.WriteLine("Dessa är dina quotes!");
+        Console.WriteLine("");
         for (int i = player.Quotes.Count - 1; i >= 0; i--)
 {
     Console.WriteLine(player.Quotes[i]);
 }
+    Console.ReadLine();
     }
 
 
@@ -91,6 +102,7 @@ skriv in siffran på det du vill ska hända!
     {
         while (true)
         {
+            Console.Clear();
             Console.WriteLine("skriv in vad du vill säga för quotes! skriv avsluta om du vill tillbaka till menyn");
             string QuoteVal = Console.ReadLine().ToLower();
             if (QuoteVal == "avsluta")
@@ -114,7 +126,9 @@ skriv in siffran på det du vill ska hända!
     }
     else
     {
-        Console.WriteLine($"{MenyVal}: Det där är inget alternativ!");
+        Console.Clear();
+        Console.WriteLine($"({MenyVal}) är inget alternativ!");
+        Thread.Sleep(1500);
     }
 
 }
